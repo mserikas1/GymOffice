69599f3d2e8265e40095adc14f82c878a2d9695f
Pavlov-Vladimir <bob.pavlov.a7@gmail.com>
2022-09-16 18:01:31 +0300

    Create WebAdmin project

---
+++ GymOffice.WebAdmin/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using MudBlazor.Services;
+global using GymOffice.WebAdmin.Data;
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Web;
+global using GymOffice.DataAccess.SQL;
+
+
