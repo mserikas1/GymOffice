15d2b8665608014c0121ee32ae30659e032dd43c
Pavlov-Vladimir <bob.pavlov.a7@gmail.com>
2022-09-17 16:13:24 +0300

    made menu change depending on the path

--- GymOffice.WebAdmin/GlobalUsings.cs
+++ GymOffice.WebAdmin/GlobalUsings.cs
@@ -3,5 +3,5 @@ global using GymOffice.WebAdmin.Data;
 global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Components.Web;
 global using GymOffice.DataAccess.SQL;
-
-
+global using GymOffice.Common.DTOs;
+global using System.ComponentModel.DataAnnotations;
