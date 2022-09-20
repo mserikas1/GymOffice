ff8a31454a72d96d5cc07e2e5a619fed02dd53af
Ferum2704 <protsysyn.bohdan@gmail.com>
2022-09-20 17:07:15 +0300

    Created CustomerApi

---
+++ GymOffice.CustomerApi/Controllers/CoachController.cs
@@ -0,0 +1,17 @@
+﻿using GymOffice.DataAccess.SQL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymOffice.CustomerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoachController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public CoachController(ApplicationDbContext context)
+        {
+           _context= context;
+        }
+    }
+}
