11a66926e1c435251414e90f2a25086fcbd26748
Ferum2704 <protsysyn.bohdan@gmail.com>
2022-10-10 14:54:24 +0300

    fixed carousel photos display

--- GymOffice.VisitorCabinet/ClientApp/src/components/HomePageComponents/PhotoCarousel.js
+++ GymOffice.VisitorCabinet/ClientApp/src/components/HomePageComponents/PhotoCarousel.js
@@ -3,30 +3,31 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function PhotoCarousel() {
-    const [photos, setPhotos] = useState([]);
-    const url = "carouselphoto/getcarouselphotos";
+  const [photos, setPhotos] = useState([]);
+  const url = "carouselphoto/getcarouselphotos";
 
-    useEffect(() => {
-        axios.get(url).then((response) => setPhotos(response.data));
-    }, []);
+  useEffect(() => {
+    axios.get(url).then((response) => setPhotos(response.data));
+  }, []);
 
-    return (
-        <div className="my-5 w-75 mx-auto row">
-            {Array.isArray(photos) ? (
-                <Carousel>
-                    { photos.map((url) => (
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100 mx-auto"
-                                src={url}
-                                alt="slide"
-                            />
-                        </Carousel.Item>
-                    )) }
-                </Carousel>
-            ) : (
-                <h1></h1>
-            )}
-        </div>
-    );
+  return (
+    <div className="my-5 w-75 mx-auto row">
+      {Array.isArray(photos) ? (
+        <Carousel>
+          {photos.map((url) => (
+            <Carousel.Item>
+              <img
+                style={{ height: "600px" }}
+                className="d-block w-100 mx-auto"
+                src={url}
+                alt="slide"
+              />
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      ) : (
+        <h1></h1>
+      )}
+    </div>
+  );
 }
