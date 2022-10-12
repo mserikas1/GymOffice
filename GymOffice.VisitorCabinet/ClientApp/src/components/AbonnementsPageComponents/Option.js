0d9e6688aa94ba6d496ed5ba4ce73bef077ba552
Ferum2704 <protsysyn.bohdan@gmail.com>
2022-10-12 15:01:22 +0300

    fixed months to month

--- GymOffice.VisitorCabinet/ClientApp/src/components/AbonnementsPageComponents/Option.js
+++ GymOffice.VisitorCabinet/ClientApp/src/components/AbonnementsPageComponents/Option.js
@@ -2,10 +2,11 @@ import { BsArrowRight } from "react-icons/bs";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Option(props) {
+  const duration = props.duration;
   return (
     <div className="d-flex text-center fs-3 flex-row justify-content-around">
       <div className="text-black" style={{ width: "45%" }}>
-        {props.duration} months
+        {duration == 1 ? `${duration} month` : `${duration} months`}
       </div>
       <div style={{ width: "25%" }}>
         <FontAwesomeIcon icon={faArrowRight} />
